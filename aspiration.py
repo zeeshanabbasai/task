4fe2fe9d21d6dab9abd6597727dbdc1a7c85ccbe
Zeeshan Abbasi <90863519+zeeshanabbasai@users.noreply.github.com>
2021-09-17 16:11:36 +0500

    Assesrtion Added

--- aspiration.py
+++ aspiration.py
@@ -1,15 +1,7 @@
 from selenium import webdriver
 import undetected_chromedriver
-import os,random
+
 import time
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC, select
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.action_chains import ActionChains
 
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
@@ -40,11 +32,9 @@ spend_and_save_button.click()
 
 time.sleep(3)
 
-try:
-    verify_spend_and_save_button=driver.find_element_by_xpath('//div[@class="error-code"]')
-    print("\n[--]  TEST 1: Unable to Acces Page. User cant go to “Spend & Save” page \n")
-except:
-    print("\n [++] Website Working\n")
+verify_spend_and_save_button=driver.find_element_by_xpath('//div[@class="error-code"]')
+assert verify_spend_and_save_button, "\n[--]  TEST 1: Unable to Acces Page. User cant go to “Spend & Save” page \n"
+print("\n [++] Website Working\n")
 
 
 print("\n[++] TEST 2: 2 card products, Aspiration and Aspiration Plus \n" )
@@ -54,21 +44,19 @@ driver.back()
 time.sleep(2)
 
 
-try:
-    
-    aspiration_card=driver.find_element_by_xpath('//div[@id="price-card-2"]')
-    driver.execute_script("arguments[0].scrollIntoView();",aspiration_card)
-    print("\n [++] Activation Card product exists\n")
-except:
-    print("\n[--] Actication Card Not exists \n")
 
-try:    
-    aspirationplus_card=driver.find_element_by_xpath('//div[@id="price-card-3"]')
-    driver.execute_script("arguments[0].scrollIntoView();",aspirationplus_card)
-    print("\n [++] Activation PLUS Card product exists\n")
-except:
-    print("\n[--] Actication PLUS Card Not exists \n")
+aspiration_card=driver.find_element_by_xpath('//div[@id="price-card-2"]')
+assert aspiration_card , ("\n[--] Actication Card Not exists \n")
+driver.execute_script("arguments[0].scrollIntoView();",aspiration_card)
+print("\n [++] Activation Card product exists\n")
 
+
+
+
+aspirationplus_card=driver.find_element_by_xpath('//div[@id="price-card-3"]')
+assert aspirationplus_card ,"\n[--] Actication PLUS Card Not exists \n"
+driver.execute_script("arguments[0].scrollIntoView();",aspirationplus_card)
+print("\n [++] Activation PLUS Card product exists\n")
 
 
 
@@ -76,15 +64,16 @@ except:
 print("\n[++] TEST 3: Get Aspiration -  input field for an email Verification \n" )
 
 
-try:    
-    aspiration_card=driver.find_element_by_xpath('//div[@id="price-card-2"]')
-    aspiration_card.click()
-    time.sleep(1)
-    aspiration_email=driver.find_element_by_xpath('//div[@role="dialog"]//input[@aria-label="Email Address"]')
-    print("\n [++] Email Input field Exists\n")
-    driver.find_element_by_xpath('//button[@data-testid="trigger"]').click()
-except:
-    print("\n[--] Email Input field dont exists \n")
+
+aspiration_card=driver.find_element_by_xpath('//div[@id="price-card-2"]')
+aspiration_card.click()
+time.sleep(1)
+aspiration_email=driver.find_element_by_xpath('//div[@role="dialog"]//input[@aria-label="Email Address"]')
+
+assert aspiration_email, "\n[--] Email Input field dont exists \n"
+print("\n [++] Email Input field Exists\n")
+driver.find_element_by_xpath('//button[@data-testid="trigger"]').click()
+
 
 
 print("\n[++] TEST 4: Get Aspiration PLUS -  modal with monthly and yearly plans appears \n" )
@@ -96,12 +85,8 @@ time.sleep(1)
 aspirationplus_month_year=driver.find_element_by_xpath('//div[@role="dialog" and not(@id="onetrust-pc-sdk")]')
 test=aspirationplus_month_year.text
 
-if "monthly" in test or "yearly" in test or "plans" in test:
-
-    print("\n [++] Yearly/Montly plans Modal can be seen\n") 
-
-else:
-    print("\n [--] Yearly/Monthly cannot be seen\n")
+assert "monthly" in test or "yearly" in test or "plans" in test,"\n [--] Yearly/Monthly cannot be seen\n"
+print("\n [++] Yearly/Montly plans Modal can be seen\n") 
 
 
 print("\n[++] TEST 5-6: Verify that yearly radio option \n" )
